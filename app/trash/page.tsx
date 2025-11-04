5802d9e3cd5a416f60288b30ec79061a8c565d49
ikechukwu powel <ikechukwupowei21@gmail.com>
2025-11-04 17:31:55 +0100

    doc: trash file created

---
+++ app/trash/page.tsx
@@ -0,0 +1,39 @@
+// .slick-dots {
+//   bottom: 10px !important;
+//   display: flex !important;
+//   justify-content: center;
+//   align-items: center;
+//   gap: 2px !important; /* very tight spacing */
+// }
+
+// .slick-dots li {
+//   width: 6px !important;   /* shrink container */
+//   height: 6px !important;
+//   margin: 0 !important;
+//   padding: 0 !important;
+// }
+
+// .slick-dots li button {
+//   width: 6px !important;
+//   height: 6px !important;
+//   padding: 0 !important;
+//   margin: 0 !important;
+//   background: transparent !important;
+//   border: none !important;
+// }
+
+// .slick-dots li button::before {
+//   content: '' !important;
+//   width: 4px !important;
+//   height: 4px !important;
+//   background: rgba(255, 255, 255, 0.6) !important;
+//   border-radius: 50% !important;
+//   opacity: 1 !important;
+//   transition: all 0.3s ease !important;
+// }
+
+// .slick-dots li.slick-active button::before {
+//   background: #fff !important;
+//   width: 5px !important;
+//   height: 5px !important;
+// }
