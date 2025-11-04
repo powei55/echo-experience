73479bc1b65a9b6901396f066e7697ae22d0e207
ikechukwu powel <ikechukwupowei21@gmail.com>
2025-11-04 17:31:24 +0100

    chore: added montserrat font to the project

--- app/utils/font.ts
+++ app/utils/font.ts
@@ -1,39 +1,16 @@
-import { Lato, Nunito_Sans, Poppins } from 'next/font/google';
+import { Libre_Baskerville, Montserrat } from "next/font/google";
 
-import { Playfair_Display, Montserrat } from 'next/font/google';
-
-export const playfairDisplay = Playfair_Display({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-playfair_display',
-  weight: ['400', '500', '600', '700', '800'],
+export const libreBaskerville = Libre_Baskerville({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-libre_baskerville",
+  weight: ["400", "700"], // regular + bold
+  style: ["normal", "italic"], 
 });
 
 export const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-montserrat',
-  weight: ['300', '400', '500', '600', '700', '800'],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
+  weight: ["300", "400", "500", "600", "700", "800"],
 });
-
-
-// export const nunitoSans = Nunito_Sans({
-//   subsets: ['latin'],
-//   display: 'swap',
-//   variable: '--font-nunito_sans',
-//   weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
-// });
-
-// export const poppins = Poppins({
-//   subsets: ['latin'],
-//   display: 'swap',
-//   variable: '--font-poppins',
-//   weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
-// });
-
-// export const lato = Lato({
-//   subsets: ['latin'],
-//   display: 'swap',
-//   variable: '--font-lato',
-//   weight: ['100', '300', '400', '700', '900'],
-// });
